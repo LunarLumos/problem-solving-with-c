f76864416c4244e80910d1f39799293e59993b82
Lunar Lumos <ariful242-16-025@diu.edu.bd>
2024-10-30 00:40:19 +0000

    Update print-sum-of-series-like-1.2+2.3+3.4+....c

--- print-sum-of-series-like-1.2+2.3+3.4+....c
+++ print-sum-of-series-like-1.2+2.3+3.4+....c
@@ -2,24 +2,23 @@
 
 int main() {
     int n;
-    printf("Enter the last number: ");
+     float sum = 0;
+
+    printf("Enter the number of terms (n): ");
     scanf("%d", &n);
-    float sum = 0;
 
-    printf("Numbers: ");
+    printf("Series: ");
     for (float i = 1.2; i <= n; i = i + 1.1) {
-        sum =sum + i; 
-        printf("%.1f", i);  // Print the number with one decimal
-
-        // Print "+" if this is not the last number
-        if (i + 1.1 <= n) {
+        sum = sum + i;
+        printf("%.1f", i);
+        if (i <= n) {
             printf(" + ");
         }
     }
-    printf("\n");
-
-    printf("The sum of numbers from 1.2 to %d by gaping 1.1 is: %.2f\n", n, sum);
+    
+    printf(" = %.1f\n", sum);
     return 0;
 }
 
-//1.2+2.3+3.4+....
+
+//1.2+2.3+3.4+...
