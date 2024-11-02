d295e83e9ac17222e7dda98edec144803aa9d36f
Lunar Lumos <ariful242-16-025@diu.edu.bd>
2024-11-02 05:39:18 +0000

    Update print-sum-of-series-like-1.2+2.3+3.4+....c

--- print-sum-of-series-like-1.2+2.3+3.4+....c
+++ print-sum-of-series-like-1.2+2.3+3.4+....c
@@ -2,23 +2,21 @@
 
 int main() {
     int n;
-     float sum = 0;
+    float a = 1.2, sum = 0;
 
-    printf("Enter the number of terms (n): ");
-    scanf("%d", &n);
+    printf("Enter the last number of this seris : ");
+    scanf("%d", &n); 
 
-    printf("Series: ");
-    for (float i = 1.2; i <= n; i = i + 1.1) {
-        sum = sum + i;
-        printf("%.1f", i);
-        if (i <= n) {
-            printf(" + ");
+    for (;;) {
+        printf(" %.1f",a);
+        sum =sum + a;
+        a=a+1.1; 
+        if (a > n) {
+            break; 
         }
     }
-    
-    printf(" = %.1f\n", sum);
+
+    printf("\nSum of 1 to %d = %.1f\n", n, sum);
     return 0;
 }
-
-
 //1.2+2.3+3.4+...
