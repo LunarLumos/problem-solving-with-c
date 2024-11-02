94abe69139348cbd2e5de3adbbc13af66db386e1
Lunar Lumos <ariful242-16-025@diu.edu.bd>
2024-11-02 04:50:46 +0000

    Update print-sum-of-series-like-4+12+20+28+….c

--- print-sum-of-series-like-4+12+20+28+….c
+++ print-sum-of-series-like-4+12+20+28+….c
@@ -21,3 +21,31 @@ int main() {
 
 
 //4+12+20+...
+
+
+// 0r 
+
+#include <stdio.h>
+
+int main() {
+    int a = 4, n, sum = 0, count = 0;
+
+    printf("Enter the number of terms (n): ");
+    scanf("%d", &n);
+
+    for (;;) {
+        printf ("%d ",a);
+        if (a + 8 <= n) {
+            printf(" + ");
+        }
+        sum = sum + a;
+        a = a + 8;
+        count++;
+        if (count >= n) {
+            break;
+        }
+    }
+
+    printf("\nSum = %d\n", sum);
+    return 0;
+}
